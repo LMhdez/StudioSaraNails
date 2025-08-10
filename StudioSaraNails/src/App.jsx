ad4066a8ee807e4250ecfe316fe9b3c86fb4f840
lhernandez <luismariohernandezsantos@gmail.com>
2025-08-10 01:43:56 +0100

    image test

--- StudioSaraNails/src/App.jsx
+++ StudioSaraNails/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <div>
         <TopBar></TopBar>
-        
+        <img src="https://zbcinljpwlqaiskgcres.supabase.co/storage/v1/object/public/images/nails/bluenailsSara.jpg" alt="" srcset="" />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
